ce9c4f36787a4b213ea3fff2925e7246a66dd906
tmdgus95 <8571053@naver.com>
2023-04-18 19:46:34 +0900

    헤더 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+
+const ButtonStlye = 'bg-slate-400 p-2 rounded-lg mx-2';
+export default function Header() {
+    const navigate = useNavigate();
+    return (
+        <div className='flex w-full px-4 justify-between items-center mt-4'>
+            <div>
+                <p className='text-3xl font-semibold'>
+                    원티드 프리온보딩 프론트엔드 - 선발 과제
+                </p>
+            </div>
+            <div>
+                <button
+                    className={ButtonStlye}
+                    onClick={() => navigate('/signup')}
+                >
+                    회원가입
+                </button>
+                <button
+                    className={ButtonStlye}
+                    onClick={() => navigate('/signin')}
+                >
+                    로그인
+                </button>
+                <button
+                    className={ButtonStlye}
+                    onClick={() => navigate('/signout')}
+                >
+                    로그아웃
+                </button>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Todolist from './pages/Todolist';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <NotFound />,
+        children: [
+            { index: true, element: <Todolist /> },
+            { path: '/signup', element: <Signup /> },
+            { path: '/signin', element: <Signin /> },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // <React.StrictMode>
+    <RouterProvider router={router} />
+    //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
