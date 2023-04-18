5bc809cd85ee146835a669c4094988fa7a0924e3
tmdgus95 <8571053@naver.com>
2023-04-19 00:03:29 +0900

    Assignment 10 클리어
    구현은 체크박스 제외하고 다했는데... 디자인이랑 체크박스가 맘에 걸린다.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,8 @@ import { Instance } from '../api/axios';
 
 export default function Todo({ todo: { todo, isCompleted, id }, setTodos }) {
     const [checked, setChecked] = useState(isCompleted);
+    const [editTodo, setEditTodo] = useState('');
+    const [editMode, setEditMode] = useState(false);
     const token = localStorage.getItem('token');
     const handleDelete = () => {
         const header = {
@@ -16,20 +18,72 @@ export default function Todo({ todo: { todo, isCompleted, id }, setTodos }) {
             });
         });
     };
+    const handleUpdate = () => {
+        const header = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const body = {
+            todo: editTodo,
+            isCompleted: checked,
+        };
+        Instance.put(`/todos/${id}`, body, header).then((res) => {
+            Instance.get('todos', header).then((res) => {
+                setTodos(res.data);
+            });
+            setEditMode(false);
+        });
+    };
     return (
-        <li>
-            <label htmlFor=''>
-                <input
-                    type='checkbox'
-                    checked={checked}
-                    onChange={() => setChecked((prev) => !prev)}
-                />
-                {todo}
-            </label>
-            <button data-testid='modify-button'>수정</button>
-            <button data-testid='delete-button' onClick={handleDelete}>
-                삭제
-            </button>
-        </li>
+        <>
+            {!editMode && (
+                <li>
+                    <label htmlFor=''>
+                        <input
+                            type='checkbox'
+                            checked={checked}
+                            onChange={() => setChecked((prev) => !prev)}
+                        />
+                        {todo}
+                    </label>
+                    <button
+                        data-testid='modify-button'
+                        onClick={() => setEditMode(!editMode)}
+                    >
+                        수정
+                    </button>
+                    <button data-testid='delete-button' onClick={handleDelete}>
+                        삭제
+                    </button>
+                </li>
+            )}
+            {editMode && (
+                <li>
+                    <label htmlFor=''>
+                        <input
+                            type='checkbox'
+                            checked={checked}
+                            onChange={() => setChecked((prev) => !prev)}
+                        />
+                        <input
+                            data-testid='modify-input'
+                            type='text'
+                            defaultValue={todo}
+                            onChange={(e) => setEditTodo(e.target.value)}
+                        />
+                    </label>
+                    <button data-testid='submit-button' onClick={handleUpdate}>
+                        제출
+                    </button>
+                    <button
+                        data-testid='cancel-button'
+                        onClick={() => setEditMode(!editMode)}
+                    >
+                        취소
+                    </button>
+                </li>
+            )}
+        </>
     );
 }
